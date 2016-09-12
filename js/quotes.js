1065aa24c9cba5e062ac5d1be34b1fd101f0e8fa
Mason Embry <masonembry81@mac.com>
2016-09-12 13:02:36 -0500

    Added list of quotes and random quote functions.

---
+++ js/quotes.js
@@ -0,0 +1,62 @@
+// List of quotes as an array of objects.
+
+var quotes = [
+	{
+		quote: 'I haven\'t a particle of confidence in a man who has no redeeming petty vices whatsoever.',
+		source: 'Mark Twain',
+		citation: '"Answers to Correspondents," The Californian',
+		year: 1865
+	},
+	{
+		quote: 'Before working smarter, give working harder a try.',
+		source: 'Michael Whittaker',
+		year: 2013
+	},
+	{
+		quote: 'If it\s worth doing, it\s worth over-doing.',
+		source: 'Mason Embry',
+		citation: '',
+		year: 1999
+	},
+	{
+		quote: 'Instead of wondering when your next vacation is, maybe you should set up a life you don\'t need to escape from.',
+		source: 'Seth Godin',
+	},
+	{
+		quote: 'Placeholder 1 is the best placeholder ever.',
+		source: 'Quotey McQuoterson',
+		citation: 'Brilliant Quotes of the 20th Century',
+		year: 2010
+	},
+	{
+		quote: 'Placeholder 2 is the best placeholder ever.',
+		source: 'Quotey McQuoterson',
+		citation: 'Brilliant Quotes of the 20th Century',
+		year: 2010
+	},
+	{
+		quote: 'Placeholder 3 is the best placeholder ever.',
+		source: 'Quotey McQuoterson',
+		citation: 'Brilliant Quotes of the 20th Century',
+		year: 2010
+	},
+	{
+		quote: 'Placeholder 4 is the best placeholder ever.',
+		source: 'Quotey McQuoterson',
+		citation: 'Brilliant Quotes of the 20th Century',
+		year: 2010
+	},
+	{
+		quote: 'Placeholder 5 is the best placeholder ever.',
+		source: 'Quotey McQuoterson',
+		citation: 'Brilliant Quotes of the 20th Century',
+		year: 2010
+	},
+	{
+		quote: 'Placeholder 6 is the best placeholder ever.',
+		source: 'Quotey McQuoterson',
+		citation: 'Brilliant Quotes of the 20th Century',
+		year: 2010
+	}
+	
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// Get a random quote. The high number of the Math.random method equals the length of the quotes array.
+function getRandomQuote() {
+	var randomArrayIndex = Math.floor(Math.random() * quotes.length);
+	return quotes[randomArrayIndex];
+}
+
+// Print a random quote to the 'quote-box' element.
+function printQuote() {
+	var randomQuote = getRandomQuote();
+	var stringOutput = '<p class="quote">' + randomQuote.quote + '</p>'
+	stringOutput += '<p class="source">' + randomQuote.source
+	
+	//the following two if statements check for the presence of a value for citation and year in the array.
+	if (randomQuote.citation) {
+		stringOutput += '<span class="citation"> ' + randomQuote.citation + '</span>'
+	}
+	if (randomQuote.year) {
+		stringOutput += '<span class="year"> ' + randomQuote.year + '</span> </p>'
+	}
+	
+	document.getElementById('quote-box').innerHTML = stringOutput;
+}
+
+// Call function to print the first quote onto the page.
+printQuote();
